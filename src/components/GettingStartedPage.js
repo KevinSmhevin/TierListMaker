58c00fe7645b1c218c4f52f2f8c4250b97a0d181
kevinsmhevin <kevincparas@gmail.com>
2018-10-22 21:56:50 -0700

    added a getting started page

--- src/components/GettingStartedPage.js
+++ src/components/GettingStartedPage.js
@@ -3,10 +3,9 @@ import React from 'react';
 export const GettingStartedPage = () => (
     <div className="container getting-started-container">
         <h4 className="getting-started-title">Tier List Maker lets you create your own rating list or "tier list" using the japanese S to F grading system.</h4><br></br>
-        <p>You could view other user's tier list in the "Tier List" Column</p>
-        <p>Once you login via google account, you can create your own tier list by clicking the "Create Tier List" button</p>
-        <p>You can use other users tier list by clicking on any of the tier list in the "Templates" column</p>
-        <p>You can always return to the home page by click on the Tier List Maker Banner</p>
+        <p>You could view other user's tier list in the "Tier List" Column <i class="fas fa-bars"></i></p> 
+        <p>Once you login via google account, you can create your own tier list by clicking the "Create Tier List" button <i className="fas fa-plus-circle"></i> </p>
+        <p>You can use other users tier list by clicking on any of the tier list in the "Templates" column <i class="fas fa-th-large"></i></p>
         <p>Sign in to get started!</p>
     </div>
 );
