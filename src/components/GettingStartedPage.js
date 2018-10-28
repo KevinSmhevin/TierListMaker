9bbbe324f81cb83e9edf522f5eaced9738a44e34
kevinsmhevin <kevincparas@gmail.com>
2018-10-27 18:55:09 -0700

    added getting started explanation

--- src/components/GettingStartedPage.js
+++ src/components/GettingStartedPage.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 export const GettingStartedPage = () => (
     <div className="container getting-started-container">
-        <h4 className="getting-started-title">Tier List Maker lets you create your own rating list or "tier list" using the japanese S to F grading system.</h4><br></br>
+        <h4 className="getting-started-title">Tier List Maker lets you create your own rating list or "tier list" using S to F grading system used through Japanese culture and academics.</h4>
+        <p>S is the highest grade that is given out only to the best, followed by the standard grades A, B, C, D then F.</p>
+        <br></br>
         <p>You could view other user's tier list in the "Tier List" Column <i class="fas fa-bars"></i></p> 
         <p>Once you login via google account, you can create your own tier list by clicking the "Create Tier List" button <i className="fas fa-plus-circle"></i> </p>
         <p>You can use other users tier list by clicking on any of the tier list in the "Templates" column <i class="fas fa-th-large"></i></p>
