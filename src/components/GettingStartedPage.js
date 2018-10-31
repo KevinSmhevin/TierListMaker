94285bd8f19ba7bcdaaeac9aea204a7f1f325be2
kevinsmhevin <kevincparas@gmail.com>
2018-10-30 21:33:52 -0700

    fixed the getting started page

--- src/components/GettingStartedPage.js
+++ src/components/GettingStartedPage.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 export const GettingStartedPage = () => (
-    <div className="container getting-started-container">
-        <h4 className="getting-started-title">Tier List Maker lets you create your own rating list or "tier list" using S to F grading system used through Japanese culture and academics.</h4>
-        <br></br><br></br>
+    <div className="getting-started-container">
+        <p>Tier List Maker lets you create your own "tier list" using the S to F grading system used in Japan and popularized by today's video game culture.</p>
+        <br></br>
         <p>S is the highest grade or rank. F is the Lowest</p>
         <br></br>
-        <h2>S <i class="fas fa-greater-than"></i> A <i class="fas fa-greater-than"></i> B <i class="fas fa-greater-than"></i> C <i class="fas fa-greater-than"></i> D <i class="fas fa-greater-than"></i> F</h2>
+        <h2>S <i className="fas fa-greater-than"></i> A <i className="fas fa-greater-than"></i> B <i className="fas fa-greater-than"></i> C <i className="fas fa-greater-than"></i> D <i className="fas fa-greater-than"></i> F</h2>
         <br></br>
-        <p>You could view other user's tier list in the "Tier List" Column </p> <h2><i class="fas fa-bars"></i></h2>
+        <p>You could view other user's tier list in the "Tier List" Column </p> <h2><i className="fas fa-bars"></i></h2>
         <br></br>
         <p>Once you login via google account, you can create your own tier list by clicking the "Create Tier List" button</p><h2><i className="fas fa-plus-circle"></i> </h2>
         <br></br>
-        <p>You can use other users tier list by clicking on any of the tier list in the "Templates" column </p><h2><i class="fas fa-th-large"></i></h2>
+        <p>You can use other users tier list by clicking on any of the tier list in the "Templates" column </p><h2><i className="fas fa-th-large"></i></h2>
         <br></br>
         <p>Sign in to get started!</p>
     </div>
