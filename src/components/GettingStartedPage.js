340807e0500b33746c8cba46013088375095d7fb
kevinsmhevin <kevincparas@gmail.com>
2018-11-02 21:37:11 -0700

    added some description to the getting started page

--- src/components/GettingStartedPage.js
+++ src/components/GettingStartedPage.js
@@ -4,15 +4,17 @@ export const GettingStartedPage = () => (
     <div className="getting-started-container">
         <p>Tier List Maker lets you create your own "tier list" using the S to F grading system used in Japan and popularized by today's video game culture.</p>
         <br></br>
-        <p>S is the highest grade or rank. F is the Lowest</p>
+        <p>S is the highest grade while F is the Lowest.</p>
+        <br></br>
+        <p>A tierlist is a list of playable elements in a videogame, subjectively ranked by their respective viability in a high level competivie setting.</p>
         <br></br>
         <h2>S <i className="fas fa-greater-than"></i> A <i className="fas fa-greater-than"></i> B <i className="fas fa-greater-than"></i> C <i className="fas fa-greater-than"></i> D <i className="fas fa-greater-than"></i> F</h2>
         <br></br>
-        <p>You could view other user's tier list in the "Tier List" Column </p> <h2><i className="fas fa-bars"></i></h2>
+        <p>You could view other user's tier list in the "Tier List" Column.</p> <h2><i className="fas fa-bars"></i></h2>
         <br></br>
-        <p>Once you login via google account, you can create your own tier list by clicking the "Create Tier List" button</p><h2><i className="fas fa-plus-circle"></i> </h2>
+        <p>Once you login via google account, you can create your own tier list by clicking the "Create Tier List" button.</p><h2><i className="fas fa-plus-circle"></i> </h2>
         <br></br>
-        <p>You can use other users tier list by clicking on any of the tier list in the "Templates" column </p><h2><i className="fas fa-th-large"></i></h2>
+        <p>You can use other users tier list by clicking on any of the tier list in the "Templates" column.</p><h2><i className="fas fa-th-large"></i></h2>
         <br></br>
         <p>Sign in to get started!</p>
     </div>
