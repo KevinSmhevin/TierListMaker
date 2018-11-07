23da0b638422eb4956faac894152aa5833c5247c
kevinsmhevin <kevincparas@gmail.com>
2018-11-06 17:01:07 -0800

    added modal

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -3,8 +3,23 @@ import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import CombinedList from './CombinedList';
 import { startGetAllMyTierList, startGetAllMyUserTierList } from '../actions/tierList'
+import { GettingStartedPage } from './GettingStartedPage';
 
 export class DashboardPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        modalIsOpen: false,
+    }
+    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this)
+}
+closeModal() {
+    this.setState({modalIsOpen: false});
+}
+openModal() {
+  this.setState({modalIsOpen: true})
+}
   onClick = () => {
     this.props.startGetAllMyTierList();
     this.props.startGetAllMyUserTierList();
@@ -13,8 +28,9 @@ export class DashboardPage extends React.Component {
     return (  
       <div>
         <NavLink to="/create" className="create side button"> <i className="fas fa-plus-circle"></i> Create Tier List</NavLink>
-        <NavLink to="/getting-started" className="side create button"><i className="fas fa-question-circle"></i> Getting Started</NavLink>
+        <button className="create side-button" onClick={this.openModal}><i className="fas fa-question-circle"></i> Help</button>
           <CombinedList />
+          <GettingStartedPage modalIsOpen={this.state.modalIsOpen} closeModal={this.closeModal}/>
       </div>
     )
   }
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,13 +2,43 @@ import React from 'react';
 import Header from './Header';
 import { NavLink } from 'react-router-dom';
 import CombinedList from './CombinedList';
+import { GettingStartedPage } from './GettingStartedPage';
 
-const LandingPage = () => (
-  <div>
+
+// const LandingPage = () => (
+//   <div>
+//     <Header />
+//     <NavLink to="/getting-started" className="getting-started create side button"><i className="fas fa-question-circle"></i> Getting Started</NavLink>
+//     <CombinedList />
+//   </div>
+// );
+
+// export default LandingPage;
+
+export default class LandingPage extends React.Component {
+  constructor() {
+            super();
+            this.state = {
+                modalIsOpen: true,
+            }
+            this.closeModal = this.closeModal.bind(this);
+            this.openModal = this.openModal.bind(this)
+
+        }
+        closeModal() {
+            this.setState({modalIsOpen: false});
+        }
+        openModal() {
+          this.setState({modalIsOpen: true})
+        }
+  render() {
+    return (
+    <div>
     <Header />
-    <NavLink to="/getting-started" className="getting-started create side button"><i class="fas fa-question-circle"></i> Getting Started</NavLink>
+    <button className="create side button" onClick={this.openModal}><i className="fas fa-question-circle"></i>Getting Started</button>
     <CombinedList />
-  </div>
-);
-
-export default LandingPage;
+    <GettingStartedPage modalIsOpen={this.state.modalIsOpen} closeModal={this.closeModal}/>
+    </div>
+  )
+  } 
+}
